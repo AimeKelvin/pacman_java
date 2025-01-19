ab10d186995493a146f4bb01110c8dc39b1706b1
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-19 09:59:10 +0200

    added our pacman class to the window

--- src/App.java
+++ src/App.java
@@ -1,10 +1,21 @@
 import javax.swing.JFrame;
 public class App{
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame("Hello World");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+        JFrame frame = new JFrame("Pacman Java by Kelvin");
         frame.setVisible(true);
-        
+        frame.setSize(boardWidth, boardHeight);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        PacMan pacManGame = new PacMan();
+        frame.add(pacManGame);
+        frame.pack();
+        frame.setVisible(true);
+
     }
 }
---
+++ src/PacMan.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Random;
+import java.swing.*;
+
+public class PacMan extends JPanel{
+int rowCount = 21;
+       private int columnCount = 19;
+       private int tileSize = 32;
+       private int boardWidth = columnCount * tileSize;
+       private int boardHeight = rowCount * tileSize;
+       PacMan(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setBackground(Color.BLACK);
+       }
+}

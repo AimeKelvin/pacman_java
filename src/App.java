9b558a09649e4ae77dfc2f233341170f94508473
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-19 09:46:53 +0200

    successfully setup my pacman project in vscode

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class App{
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("Hello World");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+        
+    }
+}

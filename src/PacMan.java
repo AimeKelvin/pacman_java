37ec68e0cc7fa61c6b540b5de5444bbb2846f41a
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-01-19 10:16:40 +0200

    loading images

--- src/PacMan.java
+++ src/PacMan.java
@@ -10,8 +10,32 @@ int rowCount = 21;
        private int tileSize = 32;
        private int boardWidth = columnCount * tileSize;
        private int boardHeight = rowCount * tileSize;
+
+private Image wallImage;
+private Image blueGhostImage;
+private Image orangeGhostImage;
+private Image pinkGhostImage;
+private Image redGhostImage;
+
+private Image pacmanLeftImage;
+private Image pacmanRightImage;
+private Image pacmanUpImage;
+private Image pacmanDownImage;
+
        PacMan(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.BLACK);
+
+        //load images
+        wallImage = new ImageIcon(getClass().getResource("./wall.png")).getImage();
+        blueGhostImage = new ImageIcon(getClass().getResource("./blueGhost.png")).getImage();
+        redGhostImage = new ImageIcon(getClass().getResource("./redGhost.png")).getImage();
+        pinkGhostImage = new ImageIcon(getClass().getResource("./pinkGhost.png")).getImage();
+        orangeGhostImage = new ImageIcon(getClass().getResource("./orangeGhost.png")).getImage();
+
+        pacmanLeftImage = new ImageIcon(getClass().getResource("./pacmanLeft.png")).getImage();
+        pacmanRightImage = new ImageIcon(getClass().getResource("./pacmanRight.png")).getImage();
+        pacmanUpImage = new ImageIcon(getClass().getResource("./pacmanUp.png")).getImage();
+        pacmanDownImage = new ImageIcon(getClass().getResource("./pacmanDown.png")).getImage();
        }
 }
